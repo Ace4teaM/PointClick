8a8006615425ec171ba84edff190b37910135aff
Thomas Auguey <thomas.auguey@hotmail.com>
2025-11-29 10:43:12 +0100

    ajout bruitage de pas

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] footstepClips;
+
+    public void PlayFootstep()
+    {
+        if (footstepClips.Length == 0) return;
+
+        AudioClip clip = footstepClips[Random.Range(0, footstepClips.Length)];
+        audioSource.PlayOneShot(clip);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
