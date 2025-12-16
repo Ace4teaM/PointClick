ec9cb9fd887d0f74dfb3acc0f884daeee57e3050
Thomas Auguey <thomas.auguey@hotmail.com>
2025-12-16 14:13:19 +0100

    ajoute l'état initial de seb

---
+++ Assets/Scripts/RestoreStatesActive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/RestoreStatesActive")]
+public class RestoreStatesActive : RestorableStates
+{
+    public override string ID => "Enable";
+
+    public override string CaptureState(GameObject obj)
+    {
+        return obj.activeSelf.ToString();
+    }
+
+    public override void RestoreState(GameObject obj, string state)
+    {
+        obj.SetActive(bool.Parse(state));
+    }
+}
